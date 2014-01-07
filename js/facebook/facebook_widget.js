18707f3dfb8180a6382d163f0339444a886612e9
jadekler <jadekler@gmail.com>
2014-01-06 21:09:31 -0500

    Added some template require.js stuff

--- js/facebook/facebook_widget.js
+++ js/facebook/facebook_widget.js
@@ -5,7 +5,20 @@ define(['jquery', 'jqueryuiwidget'], function ($) {
         },
 
         _init: function() {
+            var self = this;
+
             console.dir("facebook widget set up");
+            self.setProfileName();
+        },
+
+        setProfileName: function() {
+            var myProfileName = "My profile name!";
+
+            // do some ajax things with facebook
+            // get the person's profile name
+            // put it into myProfileName
+
+            $(this.element).children(".ps-js-profile-name").text(myProfileName);
         }
     });
 });
--- js/twitter/twitter_widget.js
+++ js/twitter/twitter_widget.js
@@ -6,6 +6,17 @@ define(['jquery', 'jqueryuiwidget'], function ($) {
 
         _init: function() {
             console.dir("twitter widget set up");
+            this.setProfileName();
+        },
+
+        setProfileName: function() {
+            var myProfileName = "My profile name!";
+
+            // do some ajax things with facebook
+            // get the person's profile name
+            // put it into myProfileName
+
+            $(this.element).children(".ps-js-profile-name").text(myProfileName);
         }
     });
 });
