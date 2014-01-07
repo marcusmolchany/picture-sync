baead4199585c8e9ba17a8129d39c1e75b8e556d
jadekler <jadekler@gmail.com>
2014-01-06 20:14:35 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <script type="text/javascript" src="/picture-sync/js/jquery/jquery-2.0.3.min.js"></script>
+        <script type="text/javascript" src="/picture-sync/js/bootstrap/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="/picture-sync/css/app.css">
+
+        <title>Jean de Klerk</title>
+    </head>
+    <body>
+        <div class="container ps-main">
+            <nav class="navbar navbar-default ps-header" role="navigation">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">Sync</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/">Boom</a></li>
+                        <li><a href="#">Bam</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            Body
+
+            <div class="ps-footer-root"></div>
+        </div>
+        <div class="ps-footer">
+            This is the footer
+        </div>
+    </body>
+</html>
