599e93c67bac072c4092d66d7650d6d11a761b03
jadekler <jadekler@gmail.com>
2014-01-06 20:25:54 -0500

    More fleshing out

--- index.php
+++ index.php
@@ -33,20 +33,20 @@
                     <div class="col-md-4">
                         <h3>Twitter</h3>
                         <img class="ps-max-width" src="img/download.png" alt="..." class="img-rounded">
-                        <div class="ps-centered">Your profile pic</div>
-                        <button type="button" class="btn btn-primary">Upload and change</button>
+                        <div class="ps-js-upload ps-upload ps-centered">Your profile pic</div>
+                        <button type="button" class="ps-max-width btn btn-primary">Upload and change</button>
                     </div>
                     <div class="col-md-4">
                         <h3>Facebook</h3>
                         <img class="ps-max-width" src="img/download.png" alt="..." class="img-rounded">
-                        <div class="ps-centered">Your profile pic</div>
-                        <button type="button" class="btn btn-success">Upload and change</button>
+                        <div class="ps-js-upload ps-upload ps-centered">Your profile pic</div>
+                        <button type="button" class="ps-max-width btn btn-success">Upload and change</button>
                     </div>
                     <div class="col-md-4">
                         <h3>Instagram?</h3>
                         <img class="ps-max-width" src="img/download.png" alt="..." class="img-rounded">
-                        <div class="ps-centered">Your profile pic</div>
-                        <button type="button" class="btn btn-danger">Upload and change</button>
+                        <div class="ps-js-upload ps-upload ps-centered">Your profile pic</div>
+                        <button type="button" class="ps-max-width btn btn-danger">Upload and change</button>
                     </div>
                 </div>
             </div>
